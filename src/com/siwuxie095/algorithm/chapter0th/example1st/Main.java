eda9d3e8883681f7e04137d1d00854e5c27a26fd
siwuxie095 <834879583@qq.com>
2021-08-08 23:31:51 +0800

    学习算法和刷题的思路指南；

---
+++ src/com/siwuxie095/algorithm/chapter0th/example1st/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.algorithm.chapter0th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-08-08 08:08:08
+ */
+public class Main {
+
+    /**
+     * 学习算法和刷题的思路指南
+     *
+     * 这⾥讲的都是普通的数据结构，用来解决常规的问题。通过 110 多道 LeetCode 算法问题，
+     * 对数据结构和算法建⽴⼀个框架性的认识。
+     *
+     * 从整体到细节，⾃顶向下，从抽象到具体的框架思维是通⽤的，不只是学习数据结构和算法，
+     * 学习其他任何知识都是⾼效的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
