e2a99ac08c4df90e55bc9d7571007a3387f1f6ee
siwuxie095 <834879583@qq.com>
2021-08-16 22:31:08 +0800

    四、总结；

---
+++ src/com/siwuxie095/algorithm/chapter0th/example5th/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.algorithm.chapter0th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-08-16 22:28:57
+ */
+public class Main {
+
+    /**
+     * 四、总结
+     *
+     * 数据结构的基本存储⽅式就是链式和顺序两种，基本操作就是增删查改，遍历⽅式⽆⾮迭代（线性）和递归（非线性）。
+     *
+     * 刷算法题建议从「树」分类开始刷，结合框架思维，把这⼏⼗道题刷完，对于树结构的理解应该就到位了。这时候去看
+     * 回溯、动规、分治等算法专题，对思路的理解可能会更加深刻⼀些。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
