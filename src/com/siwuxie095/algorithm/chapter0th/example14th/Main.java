74dc5ee62699c69d2a246e2b878bb8aad2269f9f
siwuxie095 <834879583@qq.com>
2021-08-19 08:51:13 +0800

    BFS 算法详解；

---
+++ src/com/siwuxie095/algorithm/chapter0th/example14th/Main.java
@@ -0,0 +1,27 @@
+package com.siwuxie095.algorithm.chapter0th.example14th;
+
+/**
+ * @author Jiajing Li
+ * @date 2021-08-19 08:45:23
+ */
+public class Main {
+
+    /**
+     * BFS 算法详解
+     *
+     * BFS 的核⼼思想应该不难理解的，就是把⼀些问题抽象成图，从⼀个点开始，向四周开始扩散。⼀般来说，
+     * 写 BFS 算法都是⽤「队列」这种数据结构，每次将⼀个节点周围的所有节点加⼊队列。
+     *
+     * PS：DFS 算法其实就是回溯算法。
+     *
+     * BFS 相对 DFS 的最主要的区别是：BFS 找到的路径⼀定是最短的，但代价就是空间复杂度⽐ DFS ⼤很
+     * 多，⾄于为什么，介绍了框架就很容易看出来了。
+     *
+     * 后续就将由浅⼊深写两道 BFS 的典型题⽬，分别是「⼆叉树的最⼩⾼度」和「打开密码锁的最少步数」，
+     * ⼿把⼿教你怎么写 BFS 算法。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
